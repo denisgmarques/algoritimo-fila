f2be8b0fe3ad6bfd891c6dc7c00b7aa172ecc41e
Denis Giovan Marques <denis.marques@icolabora.com.br>
2019-04-03 16:43:49 -0300

    Fixes

--- src/main/java/br/com/icolabora/queue/spreading/SpreadProcessInstances.java
+++ src/main/java/br/com/icolabora/queue/spreading/SpreadProcessInstances.java
@@ -1,3 +1,5 @@
+package br.com.icolabora.queue.spreading;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,15 +10,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /*
- * A simple program that reads a text file line-by-line using Java8.
  * @author Denis Giovan Marques
  */
 public class SpreadProcessInstances {
 
-    private int numberOfQueues = 10;
+    private int numberOfQueues = 4;
     private final String characters = "1234567890abcdef";
     private Map<String, Integer> queueByChar = new HashMap<>();
-    private String fileName = "ids-procinst-oi-homologa.txt";
+    private String fileName = "ids.txt";
     private Map<String, Integer> instancesByQueue = new HashMap<>();
 
     public static void main(String[] args) {
@@ -46,16 +47,11 @@ public class SpreadProcessInstances {
         try {
             Stream<String> lines = Files.lines(Paths.get(this.fileName));
 
-            System.out.println("<!-----Read all lines as a Stream-----!>");
+            System.out.println("<!-----Spreading all distinct lines into queues-----!>");
 
-            String lastLine = "";
-
-            for (String line: lines.collect(Collectors.toList())) {
-                if (!line.equals(lastLine)) {
-                    System.out.print(line + " - ");
-                    System.out.println(getQueueForLastCharacter(line));
-                    lastLine = line;
-                }
+            for (String line: lines.distinct().collect(Collectors.toList())) {
+                System.out.print(line + " - ");
+                System.out.println(getQueueForLastCharacter(line));
             }
 
             lines.close();
@@ -65,7 +61,7 @@ public class SpreadProcessInstances {
     }
 
     private String getQueueForLastCharacter(String pid) {
-        String lastChar = pid.substring(pid.length() - 2, pid.length() - 1);
+        String lastChar = pid.substring(pid.length() - 1);
         String queue = this.queueByChar.get(lastChar).toString();
 
         // Total of instances by queue
